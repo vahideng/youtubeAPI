76e34f4bc1b690781f53a361decc224eb24e6bd7
Vahid Davoudi <vahid_davoudi_e@yahoo.com>
2018-10-01 21:33:59 +0800

    Lodash Library is used to throttling the search function, search component is added, finished

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import VideoDetails from './components/video_details';
 import SearchBar from './components/search_bar';
+import _ from 'lodash';
 
 const API_KEY = "AIzaSyCI3HsGl3nfC99jw7oy_oItKCfzC1rxXlA";
 
@@ -19,6 +20,7 @@ class App extends Component {
     this.videoSearch("na mehraboon")
   }
 
+
   videoSearch(term) {
     YTSearch({key: API_KEY,term: term}, (videos) => {
       this.setState({
@@ -28,10 +30,10 @@ class App extends Component {
     });
   }
   render() {
-    
+const videoSearch= _.debounce((term)=>{this.videoSearch(term)},300);
     return (
       <div>
-      <SearchBar onSearchTermChanged= {term=>this.videoSearch(term)}/>
+      <SearchBar onSearchTermChanged= {videoSearch}/>
       <VideoDetails video={this.state.selectedVideo} />
       <VideoList  onClickVideo={(video)=>this.setState({selectedVideo : video})} videos={this.state.videos}/>
     </div>
